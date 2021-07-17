54e1a1e55167aa7d10ce651b2386d2fab3b7607d
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-07-17 21:59:40 +0200

    much better roller, with properly functionnal recursive 6 explosions.

--- torchbearer2_tools/roller.py
+++ torchbearer2_tools/roller.py
@@ -1,24 +1,46 @@
 import random
 
 
+class Dice:
+    """Basic dice representation, with boolean to store if it has been exploded or not."""
+
+    def __init__(self):
+        self.value = random.randint(1, 6)
+        self.exploded = False
+
+    def __repr__(self):
+        if not self.exploded and self.value == 6:
+            return f"{self.value}*"
+        return str(self.value)
+
+
 class Roller:
-    def roll(self, dice, reroll=False):
+    """Roller object. Instanciate and then use the Roller.roll() method to initiate a new roll,
+    or Roller.reroll6() method to reroll 6s from last roll."""
 
-        raw_result = [random.randint(1, 6) for die in range(dice)]
-        successes = len([die for die in raw_result if die >= 4])
+    def roll(self, num_dice, reroll=False):
+        """Basic roller function."""
+        raw_result = [Dice() for die in range(num_dice)]
+        successes = len([die for die in raw_result if die.value >= 4])
         if not reroll:
-            self.last_roll_dice = raw_result
-            self.last_roll_successes = successes
-
+            self.last_raw_result = raw_result
+            self.successes = successes
         return raw_result, successes
 
     def reroll6(self):
-        extra_dice = len([die for die in self.last_roll_dice if die == 6])
-        extra_raw_result, extra_successes = self.roll(extra_dice, reroll=True)
-        self.last_roll_dice += extra_raw_result
-        self.last_roll_successes += extra_successes
+        """This method rerolls any 6s that came out (and eventual new 6s) when characters
+        spend Persona points or use an ability allowing them to reroll 6s on their last roll."""
+        to_explode = 0
+        while True:
+            for die in self.last_raw_result:
+                if die.value == 6 and not die.exploded:
+                    die.exploded = True
+                    to_explode += 1
 
-        return (
-            self.last_roll_dice,
-            self.last_roll_successes,
-        )
+            if to_explode == 0:
+                break
+            new_raw_result, new_successes = self.roll(to_explode, reroll=True)
+            self.last_raw_result += new_raw_result
+            self.successes += new_successes
+            to_explode = 0
+        return self.last_raw_result, self.successes
