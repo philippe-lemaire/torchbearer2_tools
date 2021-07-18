b150bd9e09b15dcbca6390d95fd44b4cdf314ace
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-07-18 07:01:20 +0200

    basic implementation of base Character

--- torchbearer2_tools/character.py
+++ torchbearer2_tools/character.py
@@ -11,3 +11,17 @@ class Character:
             "Warrior": "Human",
         }
         self.stock = class_to_stock_dict[self.class_]
+        self.level = 1
+        self.belief = ""
+        self.creed = ""
+        self.instinct = ""
+        self.goal = ""
+        self.will = 0
+        self.health = 0
+        stock_to_nature_dict = {
+            "Dwarf": "Delving, Crafting, Avenging Grudges",
+            "Elf": "Singing, Remembering and Hiding",
+            "Halfling": "Sneaking, Riddling, Merrymaking",
+            "Human": "Boasting, Demanding, Running",
+        }
+        self.nature = [3, stock_to_nature_dict[self.stock]]
